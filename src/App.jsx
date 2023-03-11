b2e267696110aaefdadf1c77016bf0dfb40f15c5
DianaAkhmedova <106441711+DianaAkhmedova@users.noreply.github.com>
2023-03-11 16:59:30 +0200

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter } from 'react-router-dom';
+
+import { UserRoutes } from 'UserRoutes';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <UserRoutes />
+    </BrowserRouter>
+  );
+};
+
+export default App;
