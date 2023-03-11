22fa5e7d7ca3603aeaf634ceb2bfec66aa3e185b
DianaAkhmedova <106441711+DianaAkhmedova@users.noreply.github.com>
2023-03-11 17:01:57 +0200

    Create auth-slice.js

---
+++ src/redux/auth/auth-slice.js
@@ -0,0 +1,35 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+import { signup } from './auth-operations';
+
+const initialState = {
+  user: {},
+  token: '',
+  isLogin: false,
+  isLoading: false,
+  error: null,
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  extraReducers: builder => {
+    builder
+      .addCase(signup.pending, store => {
+        store.isLoading = true;
+        store.error = null;
+      })
+      .addCase(signup.fulfilled, (store, { payload }) => {
+        store.user = payload.user;
+        store.token = payload.token;
+        store.isLogin = true;
+        store.isLoading = false;
+      })
+      .addCase(signup.rejected, (store, { payload }) => {
+        store.isLoading = false;
+        store.error = payload;
+      });
+  },
+});
+
+export default authSlice.reducer;
