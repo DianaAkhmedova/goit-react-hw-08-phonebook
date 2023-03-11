e351760b0f80d31f2228214cd06fa0145cbce7cd
DianaAkhmedova <106441711+DianaAkhmedova@users.noreply.github.com>
2023-03-11 16:59:34 +0200

    Create App.styled.js

---
+++ src/App.styled.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const PhonebookContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 40px;
+`;
+
+export const PhonebookTitle = styled.h1`
+  margin-bottom: 20px;
+`;
+
+export const ContactsTitle = styled.h2`
+  margin-bottom: 20px;
+`;
