265b7c0cd8301a4001a05b720837a7cf44f9fa3d
DianaAkhmedova <106441711+DianaAkhmedova@users.noreply.github.com>
2023-03-11 17:01:09 +0200

    Create Layout.styled.js

---
+++ src/modules/Layout/Layout.styled.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const Header = styled.header`
+  padding: 14px 0;
+  background-color: #fafafb;
+  border-bottom: 1px solid #ececec;
+  box-shadow: 0px 1px 3px rgb(0 0 0 / 12%), 0px 1px 1px rgb(0 0 0 / 14%),
+    0px 2px 1px rgb(0 0 0 / 20%);
+`;
+
+export const Main = styled.main`
+  padding: 0px 0;
+`;
+
+export const Container = styled.div`
+  padding-left: 70px;
+  padding-right: 70px;
+  margin-left: auto;
+  margin-right: auto;
+`;
